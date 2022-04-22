5733e1d2e677e1c70f019e988fd61c0a53d593b5
HardeepSinghAu <11919@coderacademy.edu.au>
2022-04-22 20:21:46 +1000

    bundle and gems added

---
+++ calorie.rb
@@ -0,0 +1,31 @@
+require 'colorize'
+require 'tty-prompt'
+
+# Men: BMR = 88.362 + (13.397 x weight in kg) + (4.799 x height in cm) – (5.677 x age in years)
+# Women: BMR = 447.593 + (9.247 x weight in kg) + (3.098 x height in cm) – (4.330 x age in years)
+
+# welcome message
+    puts "Welcome to Calorie Counter".green
+    sleep 1 
+    puts "Please enter your name: ".yellow
+    name = gets.chomp
+
+    puts "Hello #{name}! Please enter your weight in kg: ".yellow
+    weight = gets.chomp.to_f
+
+    puts "Please enter your height in cm: ".yellow
+    height = gets.chomp.to_f
+    
+    puts "Please enter your age in years: ".yellow
+    age = gets.chomp.to_i
+
+    puts "are you male or female? (m/f): ".yellow.strip 
+    gender = gets.chomp
+    MaleBMR = ((88.362 + (13.397 * weight)) + ((4.799 * height)) - (5.677 * age))
+    FemaleBMR = ((447.593 + (9.247 * weight)) + ((3.098 * height)) - (4.330 * age))
+
+    if gender == "m"
+        puts MaleBMR
+    else
+    puts FemaleBMR
+    end
