52e9587456e4f48d6f67e0ea7422491967d3fbbd
HardeepSinghAu <11919@coderacademy.edu.au>
2022-04-30 22:43:46 +1000

    iterating calories

--- counter.rb
+++ counter.rb
@@ -5,32 +5,42 @@ prompt = TTY::Prompt.new
 
 
 
-def 
-    print_intake(array)
-    puts "Today you ate: "
-  food_array.each do |product|
-    puts "#{product}"
-  end
-end
+# def 
+#     print_intake(array)
+#     puts "Today you ate: "
+#   food_array.each do |product|
+#     puts "#{product}"
+#   end
+# end
+
+# make a hash to push user input onto
 
 
 
 begin
     def cal_count
-    food_array = []
+        
+    eats = []
+
+
     prompt = TTY::Prompt.new
 
     food_item = prompt.ask("What did you eat today? ")
     
-    if food_array.include?(food_item)
+    if eats.include?(food_item)
         puts "you have already added #{food_item} please add a new food item"
-    else food_array.push(food_item)
-    food_item_calories =  prompt.ask"how many calories is in one #{food_item}? "
+    else 
+    end
+
+    food_item_calories =  prompt.ask("how many calories is in one #{food_item}? ")
 
     print "how many #{food_item} did you eat today? "
     quantity = gets.chomp.to_i
     cal_per_food = food_item_calories.to_i * quantity
-    print "your calorie intake for #{food_item} was #{cal_per_food}  "
+    eats << {food_item => a, quantity => b, food_item_calories => c}
+
+    eats.each do |a, b, c|
+    puts "You ate #{a} lots of #{b} which is #{c} calories  "
     end
     puts "would you like to add another food item? (y/n)"
     ans = gets.chomp
@@ -41,11 +51,5 @@ begin
     end
 
 end
-
-cal_count
-
-    
 end
-
-
-# more_food 
+cal_count
--- karl.rb
+++ karl.rb
@@ -1,22 +1,37 @@
-puts "So i can tell you if your overweight ENTER YOUR WEIGHT"
-$weight = ""
-while true
-    puts "Please enter weight you jelly belly?"
-    $weight = gets.chomp
-    $weight.empty? ? (puts "Come on tell me dont be shy") : break
-end
+require 'colorize'
+require 'tty-prompt'
+require 'tty-progressbar'
+prompt = TTY::Prompt.new
 
-$weight = $weight.to_i
+# puts "So i can tell you if your overweight ENTER YOUR WEIGHT"
+# $weight = ""
+# while true
+#     puts "Please enter weight you jelly belly?"
+#     $weight = gets.chomp
+#     $weight.empty? ? (puts "Come on tell me dont be shy") : break
+# end
 
-case $weight
-  when 0..18.5
-      puts "you are underweight"
-    when 18.5..24.9
-      puts "you are normal weight"
-    when 25..29.9
-      puts "you are overweight"
-    when 30..;
-      puts "you are obese"
-    else
-      puts "invalid input"
-end
+# $weight = $weight.to_i
+
+# case $weight
+#   when 0..18.5
+#       puts "you are underweight"
+#     when 18.5..24.9
+#       puts "you are normal weight"
+#     when 25..29.9
+#       puts "you are overweight"
+#     when 30..;
+#       puts "you are obese"
+#     else
+#       puts "invalid input"
+# end
+
+  prompt = TTY::Prompt.new
+  data = []
+  a = prompt.ask("What did you eat today? ")
+  b = prompt.ask("how many calories in 1 #{a}? ")
+  c = prompt.ask("How many #{a} did you eat today? ")
+  data << {"a"=>a, "b"=>b, "c"=>c}
+  data.each do |a, b, c|
+  puts "you ate #{a} which has #{b} calories #{c}"
+  end 
