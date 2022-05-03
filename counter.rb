6d46f3c666041e9dd743e1d7d45cc3457f4bb9f1
HardeepSinghAu <11919@coderacademy.edu.au>
2022-05-04 09:56:56 +1000

    updated to counter code, no without errors

--- counter.rb
+++ counter.rb
@@ -15,14 +15,57 @@ class Total_cal
         @quantity = quantity
     end
 
-    def add_food(food_item, calories_per_food, quantity)
+#     def add_food(food_item, calories_per_food, quantity)
 
+#     eats = []
+#     foods = []
+
+#     prompt = TTY::Prompt.new
+
+#     @food_item = prompt.ask("What did you eat today? ")
+    
+#     if foods.include?(food_item)
+#         puts "you have already added #{food_item} please add a new food item"
+#     else 
+#     end
+#     foods << food_item
+
+#     food_item_calories =  prompt.ask("how many calories is in one #{food_item}? ")
+
+#     print "how many #{food_item} did you eat today? "
+#     quantity = gets.chomp.to_i
+#     cal_per_food = food_item_calories.to_i * quantity
+
+#     eats << cal_per_food
+
+
+#     eats.each do |food_item, cal_per_food|
+#         puts "you ate #{quantity} lots of #{row["food_item"]} which are #{row["cal_per_food"]} calories each "
+#     end
+
+#     puts "would you like to add another food item? (y/n)"
+#     ans = gets.chomp
+#     if ans == "y"
+#         cal_count
+#     else
+#         require_relative "index.rb"
+#     end
+
+# end
+# end
+
+
+# ----------------------------------------------------------------
+
+
+
+def add_food(food_item, calories_per_food, quantity)
     eats = []
     foods = []
 
     prompt = TTY::Prompt.new
 
-    @food_item = prompt.ask("What did you eat today? ")
+    food_item = prompt.ask("What did you eat today? ")
     
     if foods.include?(food_item)
         puts "you have already added #{food_item} please add a new food item"
@@ -40,18 +83,16 @@ class Total_cal
 
 
     eats.each do |food_item, cal_per_food|
-        puts "you ate #{quantity} lots of #{row["food_item"]} which are #{row["cal_per_food"]} calories each "
+        puts "you ate #{quantity} lots of #{"food_item"} which are #{"cal_per_food"} calories each "
     end
 
     puts "would you like to add another food item? (y/n)"
     ans = gets.chomp
     if ans == "y"
-        cal_count
+        add_food
     else
         require_relative "index.rb"
     end
-
 end
 end
-
 
